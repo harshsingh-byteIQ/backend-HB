f8f92671368df4719a635b3c6434354c0202fee5
harshsingh-byteIQ <harshsingh.byteiq@gmail.com>
2025-05-05 16:45:17 +0530

    added new db setup

--- BE/src/utils/database.py
+++ BE/src/utils/database.py
@@ -8,15 +8,20 @@ from src.configs.constants import *
 
 load_dotenv()
 
-DB_NAME = DB
-# DB_NAME = os.getenv('DB_NAME')
+DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
+DB_SSLMODE = os.getenv('DB_SSLMODE')  # Optional, if you're using SSL
 
 conn = psycopg2.connect(
-    dbname="postgres", user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=DB_PORT,
+    sslmode=DB_SSLMODE  # Optional: remove if not needed
 )
 conn.autocommit = True
 cursor = conn.cursor()
